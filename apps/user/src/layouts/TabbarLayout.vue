a000b38be17a0ff75d1ba7bfab48aa588962831a
jlbwl <ksun37754@mail.com>
2026-06-02 16:52:44 +0800

    fix: 修复员工账户做单记录图标显示问题

--- apps/user/src/layouts/TabbarLayout.vue
+++ apps/user/src/layouts/TabbarLayout.vue
@@ -34,7 +34,7 @@
       <!-- 主账户显示积分页面，员工账户显示做单记录 -->
       <van-tabbar-item
         :to="isEmployee ? '/commissions' : '/commissions'"
-        :icon="isEmployee ? 'file-text' : 'gold-coin'"
+        :icon="isEmployee ? 'list-o' : 'gold-coin'"
         name="commissions"
       >
         {{ isEmployee ? '做单记录' : '积分' }}
